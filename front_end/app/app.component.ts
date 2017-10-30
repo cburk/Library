fd48b9d37b189cfc70326cf613b2e39c0d260457
cburk <cjb6@rice.edu>
2017-10-29 21:38:56 -0500

    angular tutorial didn't include a necessary npm package lol.  In case problematic in the future, systemjs-angular-loader.js needs to be installed w/ npm install angular-loader and then changed to the correct path (node_modules/...)

--- front_end/app/app.component.ts
+++ front_end/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`
+  template: `
+  <h1>{{title}} - {{author}}</h1>
+  <h2>Checked out by: {{borrower}}</h2>
+  <h2>Owner: {{owner}}</h2>
+  `
 })
-export class AppComponent { name = 'Angular'; }
+export class BookComponent { 
+  title = 'Godel Escher Bach'; 
+  author = "Maths Guy";
+  borrower = "iggy";
+  owner = "burk";
+}
