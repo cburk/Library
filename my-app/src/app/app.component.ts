3d462e30c0e037c98aba7f908840a94d78656456
cburk <cjb6@rice.edu>
2017-12-27 16:44:34 -0800

    relearning a little css

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,26 +1,17 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common/src/directives';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'This page is Christians first (proper) angular app';
 
   ngOnInit()
   {
-    /*
-    @http({
-      method: 'GET',
-      url: 'http:localhost:8080/BookList'
-    }).then(function successCallback(response) {
-        // this callback will be called asynchronously
-        // when the response is available
-      }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
-      */
   }
 }
