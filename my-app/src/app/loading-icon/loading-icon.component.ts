e30355f199d226c7e1337dad38aa8f177acd2f76
cburk <cjb6@rice.edu>
2018-05-03 00:41:37 -0500

    inkscape lock being weird, but handmade one is sorta working

--- my-app/src/app/loading-icon/loading-icon.component.ts
+++ my-app/src/app/loading-icon/loading-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Type } from '@angular/core';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import * as anime from 'animejs';
 
@@ -10,6 +10,12 @@ import * as anime from 'animejs';
 export class LoadingIconComponent implements OnInit {
 
   isLoggedIn: boolean = true;
+  rotationalDirections = {
+    clockwise: 1,
+    widdershins: -1
+  }
+
+  wheelDirection: number
 
   constructor() { }
 
@@ -19,7 +25,8 @@ export class LoadingIconComponent implements OnInit {
 
   ngAfterViewInit() {
     console.log("ng after view init")
-    
+    this.wheelDirection = this.rotationalDirections.clockwise
+
     var svgIcon = document.getElementById("fullLockSVG") as HTMLObjectElement;
     console.log("fullLockSVG")
     console.log(svgIcon);
@@ -39,6 +46,29 @@ export class LoadingIconComponent implements OnInit {
     */
   }
 
+  private flipDirections(direction: number) {
+    if(direction == this.rotationalDirections.clockwise){
+      return this.rotationalDirections.widdershins;
+    }else{
+      return this.rotationalDirections.clockwise;
+    }
+  }
+
+  // Returns a random number between -3 and 3
+  // TODO: Have to break out flipping rotational direction into some kind of callback
+  // on completion of each turn
+  private getRandomRotation()
+  {
+    this.wheelDirection = this.flipDirections(this.wheelDirection);
+    return Math.random() * 3 * this.wheelDirection
+  }
+
+  private toTurnString(rotationDecimal)
+  {
+    console.log("Rotation: " + rotationDecimal + 'turn')
+    return rotationDecimal + 'turn'
+  }
+
   public startAnimation(): void {
     console.log("Trying to animate");
     var svgDoc = (document.getElementById("fullLockSVG") as HTMLObjectElement).getSVGDocument()
@@ -50,13 +80,16 @@ export class LoadingIconComponent implements OnInit {
 
     anime({
       targets: lockWheel,
+      /*
       translateX: [
         { value: 100, duration: 1200 },
         { value: 0, duration: 800 }
       ],
-      rotate: '1turn',
+      */
+      rotate: this.toTurnString(this.getRandomRotation()),
       backgroundColor: '#FFF',
-      duration: 2000,
+      duration: 3000,
+      easing: "easeInOutCirc",
       loop: true
     });
 
